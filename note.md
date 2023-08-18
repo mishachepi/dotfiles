73174b7179df414db5d6ac27ff3cc069f62e03ae
Mikhail Chepkin <che@CHEPKINMAC1.local>
2023-08-19 02:59:36 +0400

    add note

---
+++ note.md
@@ -0,0 +1,7 @@
+### Additional
+[runcat](https://apps.apple.com/us/app/runcat/id1429033973?mt=12)
+[karabiner-elements](https://karabiner-elements.pqrs.org/)
+glow
+bat
+neofetch
+htop
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -20,4 +20,13 @@ vim.fn.sign_define("DiagnosticSignInfo",
 vim.fn.sign_define("DiagnosticSignHint",
                    {text = "", texthl = "DiagnosticSignHint"})
 
-require("neo-tree").setup({})
+
+require('neo-tree').setup({
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    }
+  }
+})
