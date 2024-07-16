d9a8a37e2e41db9d2f2310e2006271664fe3b055
Mikhail Chepkin <mikhalchepkin@mishachepi.com>
2024-07-16 21:55:57 +0400

    update nvim plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -17,7 +17,31 @@ return {
   --     "TmuxNavigateRight",
   --     "TmuxNavigatePrevious",
   --   },
-  -- }, 
+ {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  keys = {
+    {
+      "<leader>-",
+      function()
+        require("yazi").yazi()
+      end,
+      desc = "Open the file manager",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>cw",
+      function()
+        require("yazi").yazi(nil, vim.fn.getcwd())
+      end,
+      desc = "Open the file manager in nvim's working directory" ,
+    },
+  },
+    opts = {
+      open_for_directories = false,
+    },
+  },
+
   {
     "kdheepak/lazygit.nvim",
     cmd = {
