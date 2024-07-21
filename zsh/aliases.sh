c6f4c65bb6153134c1c0baa2e5960c55f6978784
Mikhail Chepkin <mihchepi@gmail.com>
2024-07-21 16:13:55 +0400

    update zsh

---
+++ zsh/aliases.sh
@@ -0,0 +1,10 @@
+# Aliases
+alias home="$EDITOR ~/.oh-my-zsh"
+alias config="$EDITOR ~/.config"
+alias tf="terraform"
+
+alias note="$EDITOR ~/notes/."
+
+## k8s
+alias k="kubectl"
+alias kgp="kubectl get pods"
