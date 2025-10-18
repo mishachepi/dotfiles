eb3402890e17e4c4539add69720397f41c10fdb0
Mikhail Chepkin <mihchepi@gmail.com>
2025-10-18 11:36:22 +0200

    update lsp config

--- nvim/lua/mch/plugins/lsp/lspconfig.lua
+++ nvim/lua/mch/plugins/lsp/lspconfig.lua
@@ -7,17 +7,11 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
-		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import cmp-nvim-lsp plugin
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
+		local lspconfig = require("lspconfig") -- import lspconfig plugin
+		local cmp_nvim_lsp = require("cmp_nvim_lsp") -- import cmp-nvim-lsp plugin
 		local keymap = vim.keymap -- for conciseness
 
+		-- Create the autocmd for LSP keybindings
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
@@ -30,7 +24,9 @@ return {
 				keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
 				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+				keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
 
 				opts.desc = "Show documentation for what is under cursor"
 				keymap.set("n", "gh", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
@@ -51,18 +47,29 @@ return {
 				keymap.set("n", "<leader>sr", vim.lsp.buf.rename, opts) -- smart rename
 
 				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+				keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>ds", vim.diagnostic.open_float, opts) -- show diagnostics for line
+				opts.desc = "Show workspace diagnostics"
+				keymap.set("n", "<leader>dr", "<cmd>Telescope diagnostics<CR>", opts) -- show diagnostics for workspace
+
+				opts.desc = "Diagnostics Line show"
+				keymap.set("n", "<leader>df", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-				keymap.set("n", "<leader>K", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+				keymap.set("n", "<leader>dp", function()
+					vim.diagnostic.jump({ count = -1, float = true })
+				end, opts)
+				keymap.set("n", "<leader>K", function()
+					vim.diagnostic.jump({ count = -1, float = true })
+				end, opts)
 
 				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-				keymap.set("n", "<leader>J", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+				keymap.set("n", "<leader>dn", function()
+					vim.diagnostic.jump({ count = 1, float = true })
+				end, opts)
+				keymap.set("n", "<leader>J", function()
+					vim.diagnostic.jump({ count = 1, float = true })
+				end, opts)
 
 				-- Yank diagnostics message
 				keymap.set("n", "<leader>dy", function()
@@ -100,97 +107,147 @@ return {
 				end
 
 				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>dr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				keymap.set("n", "<leader>lr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 			end,
 		})
 
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		-- Update diagnostic signs in the sign column using the new API
+		local signs = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = "󰠠 ",
+		}
+
+		vim.diagnostic.config({
+			signs = {
+				text = signs,
+			},
+		})
+
+		-- #### LSP SETUP - DIRECT METHOD #### --
+		-- Define the global toggle diagnostics function
+		_G.ToggleDiagnostics = function()
+			local diagnostics_visible = vim.g.diagnostics_visible or true
+			vim.g.diagnostics_visible = not diagnostics_visible
+			if vim.g.diagnostics_visible then
+				vim.diagnostic.enable(true) -- Enable diagnostics
+				print("Diagnostics enabled")
+			else
+				vim.diagnostic.enable(false) -- Disable diagnostics
+				print("Diagnostics disabled")
+			end
 		end
 
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
+		-- Define custom root dir detection
+		local function get_python_root(startpath)
+			local git_dir = vim.fs.find({ ".git" }, { path = startpath, upward = true })[1]
+			local project_dir = vim.fs.find(
+				{ "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt" },
+				{ path = startpath, upward = true }
+			)[1]
+			return vim.fs.dirname(git_dir or project_dir or startpath)
+		end
 
-			["yamlls"] = function()
-				require("lspconfig").yamlls.setup({
-				  capabilities = capabilities,
-				  settings = {
-					yaml = {
-					  schemaStore = {
-						enable = true,
-						url = "https://www.schemastore.org/api/json/catalog.json",
-					  },
-					  schemas = {
-						["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-						["https://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
-					  },
-					  validate = true,
+		-- Pyright
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+			root_dir = get_python_root,
+			settings = {
+				python = {
+					analysis = {
+						autoSearchPaths = true,
+						diagnosticMode = "openFilesOnly",
+						useLibraryCodeForTypes = true,
 					},
-				  },
+				},
+			},
+		})
+
+		-- Ruff
+		-- lspconfig.ruff.setup({
+		-- 		capabilities = capabilities,
+		-- 		on_attach = function(client, bufnr)
+		-- 			-- disable all positional features
+		-- 			client.server_capabilities.definitionProvider = false
+		-- 			client.server_capabilities.hoverProvider = false
+		-- 			client.server_capabilities.referencesProvider = false
+		-- 			client.server_capabilities.renameProvider = false
+		-- 			client.server_capabilities.documentFormattingProvider = false
+		-- 			client.server_capabilities.codeActionProvider = false
+		-- 		end,
+		-- 		-- Optional settings - you can adjust these based on your preferences
+		-- 		settings = {
+		-- 			-- You can add specific Ruff settings here if needed
+		-- 		},
+		-- 	})
+		-- })
+
+		-- Svelte
+		lspconfig.svelte.setup({
+			capabilities = capabilities,
+			on_attach = function(client, bufnr)
+				vim.api.nvim_create_autocmd("BufWritePost", {
+					pattern = { "*.js", "*.ts" },
+					callback = function(ctx)
+						-- Here use ctx.match instead of ctx.file
+						client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+					end,
 				})
-			  end,
-
-			["bashls"] = function()
-      require("lspconfig").bashls.setup({
-        capabilities = capabilities,
-        on_attach = function(client)
-          client.server_capabilities.documentFormattingProvider = false
-        end,
-      })
-			  end,
-
-			-- ["eslint"] = function()
-			-- 	-- configure ESLint server
-			-- 	lspconfig["eslint"].setup({
-			-- 		capabilities = capabilities,
-			-- 		filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-			-- 		on_attach = function(client, bufnr)
-			-- 			vim.api.nvim_create_autocmd("BufWritePre", {
-			-- 				buffer = bufnr,
-			-- 				command = "EslintFixAll", -- Auto-fix on save
-			-- 			})
-			-- 		end,
-			-- 	})
-			-- end,
-
-			["graphql"] = function()
-			  -- configure graphql language server
-			  lspconfig["graphql"].setup({
-			    capabilities = capabilities,
-			    filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-			  })
 			end,
+		})
 
-			-- ["lua_ls"] = function()
-			-- 	-- configure lua server (with special settings)
-			-- 	lspconfig["lua_ls"].setup({
-			-- 		capabilities = capabilities,
-			-- 		settings = {
-			-- 			Lua = {
-			-- 				-- make the language server recognize "vim" global
-			-- 				diagnostics = {
-			-- 					globals = { "vim" },
-			-- 				},
-			-- 				completion = {
-			-- 					callSnippet = "Replace",
-			-- 				},
-			-- 			},
-			-- 		},
-			-- 	})
-			-- end,
+		-- TypeScript
+		lspconfig.ts_ls.setup({ -- Note: changed from ts_ls to tsserver which is the correct server name
+			capabilities = capabilities,
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"javascript.jsx",
+				"typescript",
+				"typescriptreact",
+				"typescript.tsx",
+			},
+			root_dir = function(fname)
+				return require("lspconfig.util").root_pattern("tsconfig.json")(fname)
+					or require("lspconfig.util").root_pattern("package.json", "jsconfig.json", ".git")(fname)
+			end,
+			single_file_support = true,
+			on_attach = function(client, bufnr)
+				-- Disable built-in formatting in favor of external formatters like Prettier
+				client.server_capabilities.documentFormattingProvider = false
+			end,
+		})
 
+		-- ESLint
+		lspconfig.eslint.setup({
+			capabilities = capabilities,
+			filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+			on_attach = function(client, bufnr)
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					buffer = bufnr,
+					command = "EslintFixAll", -- Auto-fix on save
+				})
+			end,
+		})
+
+		-- Lua
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					-- make the language server recognize "vim" global
+					diagnostics = {
+						globals = { "vim" },
+					},
+					completion = {
+						callSnippet = "Replace",
+					},
+				},
+			},
 		})
 	end,
 }
